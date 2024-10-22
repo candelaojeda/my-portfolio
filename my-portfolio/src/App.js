8ce337ee678947374dd5f7743af7fcf4cff9a96e
candelaojeda <ojedacandelaco@gmail.com>
2024-10-22 20:20:46 -0300

    import home component in app

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import GlobalStyles from './styles/GlobalStyles';
+import Home from './components/Home';
 
 function App() {
+  const [rotation, setRotation] = useState(0); 
+
+  const handleNextPage = () => {
+    
+    setRotation((prevRotation) => (prevRotation + 90) % 360); 
+  };
+
   return (
-    <div className="App">
-      <h1>My Portfolio</h1>
-    </div>
+    <>
+      <GlobalStyles rotation={rotation} />
+      <div className="background" style={{ transform: `rotate(${rotation}deg)`, transition: 'transform 0.5s ease' }}>
+        
+      </div>
+      {/* <div className="background" style={{ transform: `rotate(${rotation}deg)`, transition: 'transform 0.5s ease' }}>
+        
+      </div>
+      <div className="App">
+        <h1>My Portfolio</h1>
+        <button onClick={handleNextPage}>Ir a About Me</button>
+      </div> */}
+      <Home />
+    </>
   );
 }
 
