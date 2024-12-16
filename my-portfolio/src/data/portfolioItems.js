38f6a9b81de190c43f3f043f9fa754456469cedf
candelaojeda <ojedacandelaco@gmail.com>
2024-12-16 15:45:33 -0300

    added js with data information for porfolio component

---
+++ my-portfolio/src/data/portfolioItems.js
@@ -0,0 +1,143 @@
+export const projects = [
+  {
+    category: "/ PROJECTS",
+    title: "Current Personal Portfolio",
+    description: "Design and development of my personal portfolio with React.",
+    image: "/images/Home.png",
+    link: "https://github.com/candelaojeda/my-portfolio/tree/main/my-portfolio",
+  },
+  {
+    category: "/ PROJECTS",
+    title: "Previous Personal Portfolio",
+    description: "Design and development of my previous personal portfolio.",
+    image: "/images/portfolioanterior.png",
+    link: "https://playful-chebakia-348ef6.netlify.app/",
+  },
+  {
+    category: "/ PROJECTS",
+    title: "Pomodoro Timer",
+    description:
+      "Application with configuration options where you can set what work time you want to spend and what break time you need.",
+    image: "/images/pomodoro.png",
+    link: "https://github.com/candelaojeda/pomodoro/tree/master",
+  },
+  {
+    category: "/ PROJECTS",
+    title: "Shop Online",
+    description:
+      "Technical challenge that I did in the Onboarding 2021 of the Rooftop company through the Rooftop Academy.",
+    image: "/images/shopOnline.png",
+    link: "https://epic-babbage-7dbefe.netlify.app/",
+  },
+  {
+    category: "/ PROJECTS",
+    title: "Healthy Shop",
+    description:
+      "Website for healthy store. You can choose products to buy in different categories. It also has a meal plan and healthy food.",
+    image: "/images/tienda-saludable.png",
+    link: "https://vigilant-kowalevski-796973.netlify.app/",
+  },
+  {
+    category: "/ PROJECTS",
+    title: "Weather App",
+    description: "Application to know the state of the weather.",
+    image: "/images/weather-app.png",
+    link: "https://lucid-knuth-d4138c.netlify.app/",
+  },
+  {
+    category: "/ PROJECTS",
+    title: "Calculator",
+    description: "Calculator application made for a university subject.",
+    image: "/images/calculadora.png",
+    link: "https://optimistic-benz-9c7eb5.netlify.app/",
+  },
+  {
+    category: "/ PROJECTS",
+    title: "Estimate App",
+    description: "Application to estimate income and expenses.",
+    image: "/images/estimate-app.png",
+    link: "https://vigorous-bardeen-66d79f.netlify.app/",
+  },
+];
+
+export const logos = [
+  {
+    category: "/ LOGOS",
+    title: "CANDELA OJEDA",
+    description: "Personal Brand",
+    image: "/logos/cande-brand-logo.svg",
+  },
+  {
+    category: "/ LOGOS",
+    title: "FLOW",
+    description: "Children's events",
+    image: "/logos/flow-salon.svg",
+  },
+  {
+    category: "/ LOGOS",
+    title: "CANDESIGN",
+    description: "Content Creator",
+    image: "/logos/candesign (1).svg",
+  },
+  {
+    category: "/ LOGOS",
+    title: "AURA NAILS",
+    description: "Professional Manicurist",
+    image: "/logos/aura (1).svg",
+  },
+  {
+    category: "/ LOGOS",
+    title: "SENTIRES",
+    description: "Space Design",
+    image: "/logos/sentires-logo.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    category: "/ SOCIAL MEDIA",
+    title: "Instagram Feed Flow",
+    description: "Instagram feed design Flow.",
+    image: "/images/flow-sm.png",
+    link: "https://www.behance.net/gallery/200804151/Social-Media-Instagram-Feed",
+  },
+  {
+    category: "/ SOCIAL MEDIA",
+    title: "Instagram Posts Aura Nails",
+    description: "Instagram post design Aura Nails.",
+    image: "/images/aura-post-sm.png",
+    link: "https://www.instagram.com/aura.by.mili/",
+  },
+  {
+    category: "/ SOCIAL MEDIA",
+    title: "Instagram Featured Stories Aura Nails",
+    description: "Instagram feed design  Aura Nails.",
+    image: "/images/aura-stories-sm.png",
+    link: "https://www.behance.net/gallery/211276867/Logo-Design-Presentation",
+  },
+  {
+    category: "/ SOCIAL MEDIA",
+    title: "Instagram Feed Candesign",
+    description: "Instagram Feed Candesign.",
+    image: "/images/1ro.png",
+    link: "https://www.behance.net/gallery/214189877/Social-Media-Instagram-Feed",
+  },
+  {
+    category: "/ SOCIAL MEDIA",
+    title: "Invitation Card Design",
+    description: "Birthday.",
+    image: "/images/Negro50.png",
+  },
+  {
+    category: "/ SOCIAL MEDIA",
+    title: "Invitation Card Design",
+    description: "Children's Birthday.",
+    image: "/images/Invitación ISABELA.png",
+  },
+  {
+    category: "/ SOCIAL MEDIA",
+    title: "Halloween Card Design",
+    description: "Party invitation.",
+    image: "/images/halloween card.png",
+  },
+];
